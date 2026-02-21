c5f0917183583633ad91b59415deebf7d9d48767
Ilias Chymas <iliaschymas@outlook.com>
2026-02-22 01:05:42 +0200

    change default layout to vertical in telescpe and remove file preview

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -55,12 +55,16 @@ return {
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
-        -- pickers = {}
+        defaults = {
+          preview = {
+            hide_on_startup = true -- hide previewer when picker starts
+          }
+        },
+        pickers = {
+          find_files = {
+            theme = "dropdown"
+          }
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
