92bb17c669ea8ffd86973d7bbe3f9e4b7c6a2e23
IliasChymas <iliaschymas@outlook.com>
2026-03-18 18:52:39 +0200

    Change statusline

---
+++ lua/custom/plugins/lualine.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup({
+        options = {
+          theme = 'auto',                  -- or 'gruvbox', 'tokyonight', 'catppuccin', etc.
+          component_separators = { left = '', right = '' },
+          section_separators = { left = '', right = '' },
+          globalstatus = true,             -- single statusline for all windows
+        },
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = { 'filename' },
+          lualine_x = { 'encoding', 'fileformat', 'filetype' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' },
+        },
+      })
+    end,
+  },
+}
